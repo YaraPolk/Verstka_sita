fd46543723985f5b25756358bcf8c1152711fbd4
Yaroslav Ivanitskiy <yaroslav260493@gmail.com>
2018-12-27 20:44:21 +0300

    Верстка сайта7

--- js/scroll.js
+++ js/scroll.js
@@ -12,7 +12,7 @@ $(window).scroll(function() {
                 ln1.style.width = '0%';
                 bt1.style.opacity = '0';
             }
-            else if ($(this).scrollTop() < 4050) {
+            else if ($(this).scrollTop() < 4000) {
                 ln1.style.width = '0%';
                 bt1.style.opacity = '0';
             }
@@ -20,11 +20,52 @@ $(window).scroll(function() {
     }
 });
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 400) {
-            $('.block2  #kb2').css('opacity', '1');
-        }
-        if ($(this).scrollTop() > 1700) {
-            $('.block2  #kb2').css('opacity', '0');
-        }
-    });
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 350) {
+        $('.block2 #kb2').css('opacity', '1');
+        $('.block2 .svg_tx').css('width', '15%');
+        $('.block2 #bt1').css('opacity', '1');
+    }
+    if ($(this).scrollTop() > 1700) {
+        $('.block2  #kb2').css('opacity', '0');
+        $('.block2 .svg_tx').css('width', '0');
+        $('.block2 #bt1').css('opacity', '0');
+    }
+
+    if ($(this).scrollTop() < 300) {
+        $('.block2 #kb2').css('opacity', '0');
+        $('.block2 .svg_tx').css('width', '0');
+        $('.block2 #bt1').css('opacity', '0');
+    }
+    if ($(this).scrollTop() > 1700) {
+        $('.block3 #kb3').css('opacity', '1');
+        $('.block3 #y_n_g').css('right', '0');
+        $('.block3 #y_n_g').css('opacity', '1');
+    }
+    if ($(this).scrollTop() > 3100) {
+        $('.block3 #kb3').css('opacity', '0');
+        $('.block3 #y_n_g').css('right', '40%');
+        $('.block3 #y_n_g').css('opacity', '0');       
+    }
+
+    if ($(this).scrollTop() < 1400) {
+        $('.block3 #kb3').css('opacity', '0');
+        $('.block3 #y_n_g').css('right', '40%');
+        $('.block3 #y_n_g').css('opacity', '0'); 
+    }
+    if ($(this).scrollTop() > 2800) {
+        $('.block4 #kb4').css('opacity', '1');
+        $('.block4 .peole_buy').css('left', '0');
+        $('.block4 .peole_buy').css('opacity', '1');
+    }
+    if ($(this).scrollTop() > 4000) {
+        $('.block4 #kb4').css('opacity', '0');
+        $('.block4 .peole_buy').css('left', '20%');
+        $('.block4 .peole_buy').css('opacity', '0');
+    }
+    if ($(this).scrollTop() < 2600) {
+        $('.block4 #kb4').css('opacity', '0');
+        $('.block4 .peole_buy').css('left', '20%');
+        $('.block4 .peole_buy').css('opacity', '0');
+    }
+});
